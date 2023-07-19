0fd525492c82b73467d0bf31fe6889985797e2ef
Gwannon <monclus.jorge@gmail.com>
2023-07-19 14:12:01 +0200

    Mejoras en el admin de forma que haora podemos establecer un prompt o varios para cada formulario con hasta 3 respuestas posibles con su tag.

--- admin.php
+++ admin.php
@@ -10,6 +10,14 @@ function cf7cc_page_settings() {
 	?><h1><?php _e("Configuration", 'cf7cc'); ?></h1><?php 
 	if(isset($_REQUEST['send']) && $_REQUEST['send'] != '') { 
 		?><p style="border: 1px solid green; color: green; text-align: center;"><?php _e("Data saved OK!!!!!", 'cf7cc'); ?></p><?php
+		//echo "<pre>"; print_r ($_POST['_cf7cc_forms']); echo "</pre>";
+		//Limpiamos los formualrios de CF7
+		foreach ($_POST['_cf7cc_forms'] as $key => $form) {
+			if ($form['form_id'] == 0) unset($_POST['_cf7cc_forms'][$key]);
+		}
+		$_POST['_cf7cc_forms'] = array_values($_POST['_cf7cc_forms']);
+		//echo "<pre>"; print_r ($_POST['_cf7cc_forms']); echo "</pre>";
+
 		foreach ($_POST as $label => $value) {
 			if (strpos($label, "_cf7cc_") !== false) update_option($label, $value);
 		}
@@ -22,14 +30,78 @@ function cf7cc_page_settings() {
 		update_option('_cf7cc_no_tag', $_POST['_cf7cc_no_tag']);
 		update_option('_cf7cc_no_tag', $_POST['_cf7cc_no_tag']);
 		update_option('_cf7cc_send_emails', $_POST['_cf7cc_send_emails']); */
-	} ?>
+	} 
+	
+	$args = array('post_type' => 'wpcf7_contact_form', 'posts_per_page' => -1);
+	$cf7Forms = get_posts( $args );
+	
+	?>
 	<form method="post">
     <h2><?php _e("Main configuration", 'cf7cc'); ?></h2>
 		<b><?php _e("ChatGPT API key", 'cf7cc'); ?>:</b><br/>
 		<input type="text" name="_cf7cc_chatgpt_api_key" value="<?php echo get_option("_cf7cc_chatgpt_api_key"); ?>" style="width: calc(100% - 20px);" /><br/>
 		<b><?php _e("Clientify API key", 'cf7cc'); ?>:</b><br/>
 		<input type="text" name="_cf7cc_clientify_api_key" value="<?php echo get_option("_cf7cc_clientify_api_key"); ?>" style="width: calc(100% - 20px);" /><br/>
-		<b><?php _e("CF7 forms ids", 'cf7cc'); ?><br/><small><?php _e("comma separated", 'cf7cc'); ?></small>:</b><br/>
+		<table>
+				<tr>
+					<th>
+						<?php _e("Form", 'cf7cc'); ?>
+					</th>
+					<th>
+						<?php _e("Email field name", 'cf7cc'); ?>:<br/><small><?php _e("use field name without brackets (your-email)", 'cf7cc'); ?></small>
+					</th>
+					<th>
+						<?php _e("Prompt", 'cf7cc'); ?>:<br/><small><?php _e("use field name between brackets ([your-message])", 'cf7cc'); ?></small>
+					</th>
+					<th>
+						<?php _e("Response 1", 'cf7cc'); ?>
+					</th>
+					<th>
+						<?php _e("Response 2", 'cf7cc'); ?>
+					</th>
+					<th>
+						<?php _e("Response 3", 'cf7cc'); ?>
+					</th>
+				</tr>
+			<?php 
+				$prompts = get_option("_cf7cc_forms"); 
+				$max = 3;
+				if(count($prompts) >= $max) $max = count($prompts) + 1;
+			
+				for($i = 0; $i < $max; $i++) { ?>
+				<tr>
+					<td style="vertical-align: top;">
+						<select name="_cf7cc_forms[<?=$i;?>][form_id]">
+							<option value="0"><?php _e("Select CF7 form", 'cf7cc'); ?></option>
+							<?php foreach($cf7Forms as $form) { ?>
+								<option value="<?=$form->ID;?>"<?php echo (isset($prompts[$i]['form_id']) && $form->ID == $prompts[$i]['form_id'] ? " selected='selected'" : ""); ?>><?=$form->post_title;?></option>
+							<?php } ?>
+						</select>
+					</td>
+					<td style="vertical-align: top;">
+						<input style="width: 100%;" type="text" name="_cf7cc_forms[<?=$i;?>][email_field]" value="<?php echo (isset($prompts[$i]['email_field']) ? $prompts[$i]['email_field'] : ""); ?>" />
+					</td>
+					<td style="vertical-align: top;">
+						<textarea style="width: 100%; height: 100px;" name="_cf7cc_forms[<?=$i;?>][prompt]"><?php echo (isset($prompts[$i]['prompt']) ? stripslashes($prompts[$i]['prompt']) : ""); ?></textarea>
+					</td>
+					<td style="vertical-align: top;">
+						<input style="width: 100%;" type="text" name="_cf7cc_forms[<?=$i;?>][response1]" value="<?php echo (isset($prompts[$i]['response1']) ? $prompts[$i]['response1'] : ""); ?>" placeholder="<?php _e("Response", 'cf7cc'); ?>" /><br/>
+						<input style="width: 100%;" type="text" name="_cf7cc_forms[<?=$i;?>][tag1]" value="<?php echo (isset($prompts[$i]['tag1']) ? $prompts[$i]['tag1'] : ""); ?>" placeholder="<?php _e("Clientify Tag", 'cf7cc'); ?>" />
+					</td>
+					<td style="vertical-align: top;">
+						<input style="width: 100%;" type="text" name="_cf7cc_forms[<?=$i;?>][response2]" value="<?php echo (isset($prompts[$i]['response2']) ? $prompts[$i]['response2'] : ""); ?>" placeholder="<?php _e("Response", 'cf7cc'); ?>" /><br/>
+						<input style="width: 100%;" type="text" name="_cf7cc_forms[<?=$i;?>][tag2]" value="<?php echo (isset($prompts[$i]['tag2']) ? $prompts[$i]['tag2'] : ""); ?>" placeholder="<?php _e("Clientify Tag", 'cf7cc'); ?>" />
+					</td>
+					<td style="vertical-align: top;">
+						<input style="width: 100%;" type="text" name="_cf7cc_forms[<?=$i;?>][response3]" value="<?php echo (isset($prompts[$i]['response3']) ? $prompts[$i]['response3'] : ""); ?>" placeholder="<?php _e("Response", 'cf7cc'); ?>" /><br/>
+						<input style="width: 100%;" type="text" name="_cf7cc_forms[<?=$i;?>][tag3]" value="<?php echo (isset($prompts[$i]['tag3']) ? $prompts[$i]['tag3'] : ""); ?>" placeholder="<?php _e("Clientify Tag", 'cf7cc'); ?>" />
+					</td>
+				</tr>
+			<?php } ?>
+		</table>
+		
+		
+		<?php /* <b><?php _e("CF7 forms ids", 'cf7cc'); ?><br/><small><?php _e("comma separated", 'cf7cc'); ?></small>:</b><br/>
 		<input type="text" name="_cf7cc_forms_ids" value="<?php echo get_option("_cf7cc_forms_ids"); ?>" style="width: calc(100% - 20px);" /><br/>
 		<b><?php _e("Field name to email", 'cf7cc'); ?>:</b><br/>
 		<input type="text" name="_cf7cc_email" value="<?php echo get_option("_cf7cc_email"); ?>" style="width: calc(100% - 20px);" /><br/>
@@ -39,8 +111,12 @@ function cf7cc_page_settings() {
 		<input type="text" name="_cf7cc_prompt" value="<?php echo get_option("_cf7cc_prompt"); ?>" style="width: calc(100% - 20px);" /><br/>
 		<b><?php _e("Clientify Tag if ChatGPT responds YES", 'cf7cc'); ?>:</b><br/>
 		<input type="text" name="_cf7cc_yes_tag" value="<?php echo get_option("_cf7cc_yes_tag"); ?>" style="width: calc(100% - 20px);" /><br/>
-		<b><?php _e("Clientify Tag if ChatGPT responds NO", 'cf7cc'); ?>:</b><br/>
-		<input type="text" name="_cf7cc_no_tag" value="<?php echo get_option("_cf7cc_no_tag"); ?>" style="width: calc(100% - 20px);" /><br/>
+		<b><?php _e("Clientify Tag if ChatGPT responds NO", 'cf7cc'); ?>:</b><br/> 
+		<input type="text" name="_cf7cc_no_tag" value="<?php echo get_option("_cf7cc_no_tag"); ?>" style="width: calc(100% - 20px);" /><br/> */ ?>
+		
+		
+		
+		
 		<b><?php _e("Emails to notify", 'cf7cc'); ?>:<br/><small>(<?php _e("comma separated", 'cf7cc'); ?>)</small></b><br/>
 		<input type="text" name="_cf7cc_send_emails" value="<?php echo get_option("_cf7cc_send_emails"); ?>" style="width: calc(100% - 20px);" /><br/>
 		<br/><input type="submit" name="send" class="button button-primary" value="<?php _e("Save", 'cf7cc'); ?>" />
--- cf7-chatgpt-clientify.php
+++ cf7-chatgpt-clientify.php
@@ -3,8 +3,8 @@
 /**
  * Plugin Name: CF7-chatGPT-Clientify
  * Plugin URI:  https://github.com/gwannon/cf7-chatgpt-clientify
- * Description: Plugin que lee los textos de los formularios de CF7 y hace preguntas de si/no a ChatGPT para meter una etiqueta u otra a Clientify. 
- * Version:     0.5
+ * Description: Plugin que lee los textos de los formularios de CF7 y hace preguntas de a ChatGPT para meter una etiqueta u otra a Clientify según la respuesta. 
+ * Version:     0.6
  * Author:      Gwannon
  * Author URI:  https://github.com/gwannon/
  * License:     GNU General Public License v2 or later
@@ -14,6 +14,8 @@
  * PHP 8.2.1
  * WordPress 6.2.2
  */
+
+ ini_set("display_errors", 1);
 
 require __DIR__ . '/vendor/autoload.php';
 use Orhanerday\OpenAi\OpenAi;
@@ -25,7 +27,7 @@ use Gwannon\PHPClientifyAPI\contactClientify;
 require __DIR__ . '/admin.php';
 
 define("CLIENTIFY_API_URL", "https://api.clientify.net/v1");
-define("CLIENTIFY_LOG_API_CALLS", true);
+define("CLIENTIFY_LOG_API_CALLS", false);
 define('CHATGPT_API_KEY', get_option("_cf7cc_chatgpt_api_key"));
 define('CLIENTIFY_API_KEY', get_option("_cf7cc_clientify_api_key"));
 
@@ -41,78 +43,77 @@ add_action('plugins_loaded', 'cf7cc_plugins_loaded', 0 );
 /* Chequeamos el envío de correos */ 
 add_action('wpcf7_before_send_mail', 'cf7cc_mail_sent' );
 function cf7cc_mail_sent( $contact_form ) { 	
-    $form_ids = explode(",", get_option("_cf7cc_forms_ids"));
-    if(in_array($contact_form->id(), $form_ids)) {
-        $submission = WPCF7_Submission::get_instance(); 
-        $posted_data = $submission->get_posted_data();
-        $prompt = cf7cc_generate_prompt ($posted_data);
-        $args = [
-            'model' => 'gpt-3.5-turbo',
-            'messages' => [
-                [
-                    "role" => "system",
-                    "content" => $prompt
-                ]
-            ],
-            'temperature' => 1.0,
-            'max_tokens' => 100,
-            'frequency_penalty' => 0,
-            'presence_penalty' => 0,
-        ];
-        $counter = 0;
-        $responses = [];
-        //echo "---".$prompt."---\n";
-        for ($i = 0; $i < 3; $i++) {
+    $forms = get_option("_cf7cc_forms"); 
+    foreach ($forms as $form) {
+
+        if($contact_form->id() == $form['form_id']) {
+            $submission = WPCF7_Submission::get_instance(); 
+            $posted_data = $submission->get_posted_data();
+            $prompt = cf7cc_generate_prompt ($posted_data, $form['prompt']);
+            $args = [
+                'model' => 'gpt-3.5-turbo',
+                'messages' => [
+                    [
+                        "role" => "system",
+                        "content" => $prompt
+                    ]
+                ],
+                'temperature' => 1.0,
+                'max_tokens' => 100,
+                'frequency_penalty' => 0,
+                'presence_penalty' => 0,
+            ];
+            $counter = 0;
+            $responses = [];
+            //echo "---".$prompt."---\n";
+            
             $open_ai = new OpenAi(CHATGPT_API_KEY);
             $chat = $open_ai->chat($args);
             $d = json_decode($chat, true);
+            $response = '';
             if(isset($d['choices'][0]['message']['content'])) {
-                $response = cf7cc_clean_chtGPT_response($d['choices'][0]['message']['content']);
-                $responses[] = $response;
-                if ($response == 'yes') $counter++;
-                else if ($response == 'no') $counter--;
-                //echo "---".$response."---\n";
+                $response = cf7cc_clean_chatGPT_response($d['choices'][0]['message']['content']);
             }
-        }
 
-        //Guardamos log
-        //file_put_contents(WP_PLUGIN_DIR . '/cf7-chatgpt-clientify/log.txt', $content."\n\n\n");
+            //Guardamos log
+            //file_put_contents(WP_PLUGIN_DIR . '/cf7-chatgpt-clientify/log.txt', $content."\n\n\n");
 
-        //Avisamos a los admins
-        $content = $prompt." -----> ".implode(", ", $responses)." ----> ".$counter;
-        //echo "---".$content."---\n";
-        $admins = explode(",", get_option("_cf7cc_send_emails"));
-        if(count($admins) > 0) {
-            foreach ($admins as $email) {
-                wp_mail(trim($email), "Chat-GPT ".get_bloginfo('url'), $content);
+            //Avisamos a los admins
+            $content = $prompt." -----> ".$response;
+            //echo "---".$content."---\n";
+            $admins = explode(",", get_option("_cf7cc_send_emails"));
+            if(count($admins) > 0) {
+                foreach ($admins as $email) {
+                    wp_mail(trim($email), "Chat-GPT ".get_bloginfo('url'), $content);
+                }
             }
-        }
 
-        //Conectamos con CLientify y metemos las etiquetas adecuadas 
-        $email_label = get_option("_cf7cc_email");
-        $email = $posted_data[$email_label];
-        if($counter > 0) $tag = get_option("_cf7cc_yes_tag"); //YES
-        else if($counter < 0) $tag = get_option("_cf7cc_no_tag");
-        else $tag = "";
-        $contact = new contactClientify($email, true); //Si no existe se crea
-        if($tag != '' && !$contact->hasTag($tag)) {
-            //echo "---Añadir tag: ".$tag."---\n";
-            $contact->addTag($tag);
-            $contact->update();
+            //Conectamos con Clientify y metemos las etiquetas adecuadas 
+            $email_label = $form['email_field'];
+            $email = $posted_data[$email_label];
+            if ($form['response1'] == $response) $tag = $form['tag1'];
+            else if ($form['response2'] == $response) $tag = $form['tag2'];
+            else if ($form['response3'] == $response) $tag = $form['tag3'];
+            else $tag = "";
+            $contact = new contactClientify($email, true); //Si no existe se crea
+            if($tag != '' && !$contact->hasTag($tag)) {
+                //echo "---Añadir tag: ".$tag."---\n";
+                $contact->addTag($tag);
+                $contact->update();
+            }
         }
     }
 }
 
-function cf7cc_clean_chtGPT_response($string) {
+function cf7cc_clean_chatGPT_response($string) {
     $string = preg_replace('/[^a-z]/i','',strtolower($string));
     return $string;
 }
 
-function cf7cc_generate_prompt ($posted_data) {
-    $prompt = get_option("_cf7cc_prompt");
-    foreach (explode(",", get_option("_cf7cc_field_name")) as $label) {
+function cf7cc_generate_prompt ($posted_data, $prompt) {
+     foreach ($posted_data as $label => $value) {
         $label = trim($label);
-        $text = str_replace(["\r\n", "\r", "\n", "\t"], ' ', trim($posted_data[$label]));
+        $text = str_replace(["\r\n", "\r", "\n", "\t"], ' ', trim($value));
         $prompt = str_replace("[".$label."]", $text, $prompt);
     }
     return $prompt;
